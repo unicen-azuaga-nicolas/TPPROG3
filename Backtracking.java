8d313c9e7bb7287f64a25a0fb1c314f5e15e1c2a
Nicolás Azuaga <nazuaga@alumnos.exa.unicen.edu.ar>
2025-06-16 14:08:36 -0300

    se agregan comentarios al código de los algoritmos

--- Backtracking.java
+++ Backtracking.java
@@ -2,6 +2,14 @@ package prog3.Trabajo.Especial;
 
 import java.util.List;
 
+/*
+ * Estrategia Backtracking:
+ * - Se genera el árbol de exploracion probando todas las posibles combinaciones de máquinas.
+ * - En cada nivel del árbol se prueba con cada máquina disponible, restando su producción al total restante.
+ * - Un estado solución es aquel que logra que piezasRestantes == 0.
+ * - Se guarda la mejor solución con menor cantidad de puestas en funcionamiento .
+ * - Se implementa poda: si piezasRestantes < 0, no se continúa con esa rama.
+ */
 public class Backtracking implements Algoritmo {
 
     private Solucion mejorSolucion;
@@ -31,7 +39,7 @@ public class Backtracking implements Algoritmo {
     }
 
     private void ejecutarRecursivo(int piezasRestantes, Solucion actual, List<Maquina> maquinas) {
-        estadosGenerados++; // <— Aumentamos el contador global
+        estadosGenerados++;
 
         // Caso base: solución exacta
         if (piezasRestantes == 0) {
@@ -41,9 +49,9 @@ public class Backtracking implements Algoritmo {
             return;
         }
 
-        // Poda por exceso
+        // Poda por exceso de producción
         if (piezasRestantes < 0) return;
-        //Poda por cantidad de máquinas usadas
+        // Poda por cantidad de máquinas usadas (no se puede mejorar más)
         if (mejorSolucion != null && actual.getPuestasEnFuncionamiento() >= mejorSolucion.getPuestasEnFuncionamiento()) return;
 
         for (Maquina m : maquinas) {
